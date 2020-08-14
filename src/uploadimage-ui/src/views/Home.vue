bcce399efce26857e8a7aa2e132ce9c887d6ee2e
Nicolas <nisoffi@gmail.com>
2020-08-14 20:48:32 -0300

    Mudanças no layout da home

--- src/uploadimage-ui/src/views/Home.vue
+++ src/uploadimage-ui/src/views/Home.vue
@@ -11,24 +11,22 @@
         <v-btn color="#0a00b6" rounded dark>Nova Obra</v-btn>
       </v-layout>
       <v-row class="mt-16" justify="space-around">
-        <v-card color="#9d46ff" dark>
+        <v-card v-for="n in 10" :key="n" color="#9d46ff" dark class="p-10">
           <v-row>
-            <v-col>
-              <v-card-title>Titulo</v-card-title>
+            <v-col  class="px-6">
+              <v-img 
+                height="200"
+                width="200"
+                src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+              ></v-img>
             </v-col>
-            <v-col>
+            <v-col justify="space-around">
               <v-card-subtitle>Descricao</v-card-subtitle>
-              <v-btn rounded outlined small dark color="#0a00b6">Apagar</v-btn>
-            </v-col>
-          </v-row>
-        </v-card>
-        <v-card color="#9d46ff" dark>
-          <v-row>
-            <v-col>
-              <v-card-title>Titulo</v-card-title>
-            </v-col>
-            <v-col>
-              <v-card-subtitle>Descricao</v-card-subtitle>
+              <v-card-actions>
+                <v-btn rounded outlined small dark color="#0a00b6"
+                  >Apagar</v-btn
+                >
+              </v-card-actions>
             </v-col>
           </v-row>
         </v-card>
@@ -46,7 +44,9 @@
 h1 {
   color: #ffffff;
 }
-
+.col-des {
+  height: 200px;
+}
 </style>
 
 <script>
