321735d22d0a11a7a117f1cafb92c7192a48056a
Nicolas <nisoffi@gmail.com>
2020-08-13 21:41:56 -0300

    primeira versao da home

--- src/uploadimage-ui/src/views/Home.vue
+++ src/uploadimage-ui/src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1 class="text-center">Upload de Imagens</h1>
+        </v-col>
+      </v-row>
+      <v-layout justify-space-between>
+        <h1>Lista de Obras de Arte</h1>
+        <v-btn color="#0a00b6" rounded dark>Nova Obra</v-btn>
+      </v-layout>
+      <v-row class="mt-16" justify="space-around">
+        <v-card color="#9d46ff" dark>
+          <v-row>
+            <v-col>
+              <v-card-title>Titulo</v-card-title>
+            </v-col>
+            <v-col>
+              <v-card-subtitle>Descricao</v-card-subtitle>
+              <v-btn rounded outlined small dark color="#0a00b6">Apagar</v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+        <v-card color="#9d46ff" dark>
+          <v-row>
+            <v-col>
+              <v-card-title>Titulo</v-card-title>
+            </v-col>
+            <v-col>
+              <v-card-subtitle>Descricao</v-card-subtitle>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.home {
+  background-color: #6200ea;
+  height: 100vh;
 }
+
+h1 {
+  color: #ffffff;
+}
+
+</style>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
 </script>
